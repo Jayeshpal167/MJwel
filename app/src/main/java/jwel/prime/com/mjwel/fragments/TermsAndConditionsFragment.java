43a7c4e1ec21876347548ab7de07fe74730462fc
mehul <gargmehulmvg@gmail.com>
2020-02-02 17:12:31 +0530

    applying constraints in Contact us and termAndCondition Fragments.

--- app/src/main/java/jwel/prime/com/mjwel/fragments/TermsAndConditionsFragment.java
+++ app/src/main/java/jwel/prime/com/mjwel/fragments/TermsAndConditionsFragment.java
@@ -1,30 +1,21 @@
 package jwel.prime.com.mjwel.fragments;
 
-
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import jwel.prime.com.mjwel.R;
+import jwel.prime.com.mjwel.adapters.PointsAdapter;
+import jwel.prime.com.mjwel.constant.ToolBarManagerHome;
+import jwel.prime.com.mjwel.model.PointsModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import jwel.prime.com.mjwel.adapters.PointsAdapter;
-import jwel.prime.com.mjwel.R;
-import jwel.prime.com.mjwel.constant.ToolBarManagerHome;
-import jwel.prime.com.mjwel.model.PointsModel;
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class TermsAndConditionsFragment extends BaseHomeFragment {
-
 
     public TermsAndConditionsFragment() {
         // Required empty public constructor
@@ -35,18 +26,18 @@ public class TermsAndConditionsFragment extends BaseHomeFragment {
     private TextView tncTitle;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         contentView = inflater.inflate(R.layout.fragment_terms_and_conditions, container, false);
         tncRecycler = contentView.findViewById(R.id.tnc_recycler);
         tncTitle = contentView.findViewById(R.id.tnc_title);
-        tncRecycler.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL, false));
+        tncRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
         List<PointsModel> list = new ArrayList<>();
         list.add(new PointsModel(1, "MJWEL is India's Leading ZERO FEE online B2B, B2C, O2O jewellery market place, bringing together Manufacture, Whole seller, Retailers and supporting them to sell their product globally."));
-        tncRecycler.setAdapter(new PointsAdapter(mActivity,list));
+        tncRecycler.setAdapter(new PointsAdapter(mActivity, list));
         return contentView;
     }
+
     @Override
     public void onStart() {
         super.onStart();
